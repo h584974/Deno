fb5775466eeb91d83bfbe745d4491118a3791545
= <=>
2022-08-26 00:10:13 +0200

    Added export of movie finder

---
+++ 250movie/index.ts.ts
@@ -0,0 +1,17 @@
+import { DOMParser } from 'deno-dom';
+
+export default async function getMovie() {
+  const res = await fetch('https://www.imdb.com/chart/top/?ref_=nv_mv_250', {
+    headers: {
+      'Accept-Language': 'en-US',
+    },
+  });
+
+  const html = await res.text();
+  const doc = new DOMParser().parseFromString(html, 'text/html')!;
+  const table = doc.querySelector('table[data-caller-name="chart-top250movie"]');
+  const list = table?.querySelectorAll('tr > td[class="titleColumn"] > a');
+  const movies: string[] = [];
+  list?.forEach((a: any) => movies.push(a.textContent));
+  return movies[Math.floor(Math.random() * movies.length)];
+}
