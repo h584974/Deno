e410392ad88a2af8a7cec943dde1a7802e006008
= <=>
2022-08-26 10:52:38 +0200

    Simplified movies project

--- 250movie/index.ts
+++ 250movie/index.ts
@@ -1,16 +1,16 @@
-import { DOMParser } from 'deno-dom';
+import { DOMParser } from "https://deno.land/x/deno_dom@v0.1.34-alpha/deno-dom-wasm.ts";
 
 export default async function getMovie() {
-  const res = await fetch('https://www.imdb.com/chart/top/?ref_=nv_mv_250', {
+  const res = await fetch("https://www.imdb.com/chart/top/?ref_=nv_mv_250", {
     headers: {
-      'Accept-Language': 'en-US',
+      "Accept-Language": "en-US",
     },
   });
 
   const html = await res.text();
-  const doc = new DOMParser().parseFromString(html, 'text/html')!;
-  const table = doc.querySelector('table[data-caller-name="chart-top250movie"]');
-  const list = table?.querySelectorAll('tr > td[class="titleColumn"] > a');
+  const doc = new DOMParser().parseFromString(html, "text/html")!;
+  const table = doc.querySelector("table[data-caller-name='chart-top250movie']");
+  const list = table?.querySelectorAll("tr > td[class='titleColumn'] > a");
   const movies: string[] = [];
   list?.forEach((a: any) => movies.push(a.textContent));
   return movies[Math.floor(Math.random() * movies.length)];
